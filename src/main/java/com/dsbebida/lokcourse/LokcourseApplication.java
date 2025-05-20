d386f8d4b5829af7f43ca093e7993c83119006f1
italo gabriel <az0990za0909909@gmail.com>
2025-05-20 17:40:17 -0300

    projeto criado

---
+++ src/main/java/com/dsbebida/lokcourse/LokcourseApplication.java
@@ -0,0 +1,13 @@
+package com.dsbebida.lokcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LokcourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LokcourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dsbebida/lokcourse/LokcourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsbebida.lokcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LokcourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
